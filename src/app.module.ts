761bc67669e5015b42b8b3880daf3fb9fe2723e1
Raphael M Carreiro <raphaelcarreiro@hotmail.com>
2021-10-27 20:59:25 -0300

    ajustes em docker-compose.yml

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 
-console.log(process.env.MONGODB_USER);
-console.log(process.env.MONGODB_PASSWORD);
-console.log(process.env.MONGODB_HOST);
-console.log(process.env.MONGODB_DATABASE);
-
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(
-      `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`,
+      `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/${process.env.MONGODB_DATABASE}?authSource=admin`,
     ),
     UsersModule,
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
